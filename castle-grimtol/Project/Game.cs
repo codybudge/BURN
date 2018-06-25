904a3af83eec8f5e681ecc0871cbc68463e49b1a
cody budge <codybudge@gmail.com>
2018-06-25 16:44:46 -0600

    finished the basic function of game!

--- castle-grimtol/Project/Game.cs
+++ castle-grimtol/Project/Game.cs
@@ -64,6 +64,28 @@ namespace CastleGrimtol.Project
 
           }
           break;
+          case "quit":
+          {
+            QuitGame();
+          }
+          break;
+          case "destroy":
+          {
+            WinGame();
+          }
+          break;
+          case "help":
+          { 
+            Help();
+          }
+          break;
+          case "look":
+          case "Look":
+          case "l":
+          {
+            Look();
+          }
+          break;
       }
 
       //get user input
@@ -80,18 +102,18 @@ namespace CastleGrimtol.Project
     public void Setup()
     {
       Console.Clear();
-      Room room0 = new Room("The Roof", " You are on the roof of a strange ship. There is this strange spire about 30ft high with a large ball at the end of it, it seems to be sucking in all of the surounding clouds.");
+      Room room0 = new Room("The Roof", " You are on the roof of a strange ship. There is this strange spire about 30ft high with a large ball at the end of it, it seems to be sucking in all of the surounding clouds. There is a button and some sort of bomb on the ground!");
       Item button = new Item("button", "a small button on a plate");
       Item fireBomb = new Item("fire bomb", "A round silver ball that you have been told will explode into fire and death!(great for parties!)");
       Item sword = new Item("sword", "Your basic hero sword, not super sharp but can withstand attacks form enemys");
       room0.Items.Add(button);
       room0.Items.Add(fireBomb);
-      Room room1 = new Room("Room 1", "You go down the hatch into room 1");
-      Room room2 = new Room("Room 2", "You go into room 2");
+      Room room1 = new Room("Room 1", "After using the button you fall into a large room with the words Room 1 on the wall, there is a door to the east and a door to the south");
+      Room room2 = new Room("Room 2", "You go through the door into another room, this one is not as big but has the same letters on the wall. This time they read room 2 on them. There is a door to the south and the door leading back to the west. There is a large sword on the ground! It's a basic hero sword!");
       room2.Items.Add(sword);
-      Room room3 = new Room("Room 3", "");
-      Room room4 = new Room("Room 4", "A small room with stairs that are going down");
-      Room room5 = new Room("Room 5", "You go into the winning room! For Now this is w");
+      Room room3 = new Room("Room 3", "You go through the door into what seems to be a closet? there isn't much in here right now but its a nice place to sit and chill for awile. the only way out is the way you came.");
+      Room room4 = new Room("Room 4", "A small with the letters Room 4 on the walls,the room has stairs that are going down");
+      Room room5 = new Room("Room 5", "You go down the stairs and there see a small lever on the wall. It has the words \"Self Destruct\" on it.type destroy to save the world!");
       // Room room6 = new Room("Room 6", "");
       // Room room7 = new Room("Room 7", "");
       // Room room8 = new Room("Room 8", "");
@@ -134,44 +156,25 @@ namespace CastleGrimtol.Project
       Playing = true;
     }
 
-public void Look(string look)
+public void Look()
 {
-
+  System.Console.WriteLine($"The items in the room are{CurrentRoom.Items}");
 }
 
     public void UseItem(string itemName)
     {
-      
-      // Item temp = CurrentPlayer.Inventory.Find(x => x.Name.Contains("fire bomb"));
-      //  System.Console.WriteLine(itemName == "button" && temp != null);
-      // if (itemName == "button" && temp != null)
-      // {
-      //   Console.WriteLine("You Die");
-      //   Playing = false;
-
-      // }
-      // else if(itemName == "fire bomb")
-      // {
-      //   System.Console.WriteLine("You look at the strange round ball in your hands. It has a small pin on it, you decide it would be a good idea to pull the pin(why...they could never find out). You can imagine what happens next. They only thing they found was a small pile of ash.....R.I.P Bob the idiot!");
-      //   Playing = false;
-      // }
-      // else
-      // {
-      //   CurrentRoom = CurrentRoom.Go("button");
-      //   System.Console.WriteLine(CurrentRoom.Name);
-      // }
       switch(itemName)
       {
         case "button":
           UseButton("button");
         break;
-        case "fire bomb":
+        case "fire":
         UseFireBomb("fire bomb");
         break;
         
-        
       }
     }
+
 
     public void UseButton(string itemName)
     {
@@ -187,9 +190,11 @@ public void Look(string look)
        }
 
     }
+
        public void UseFireBomb(string itemName)
        {
-         if(itemName == "fire bomb")
+         Item temp = CurrentPlayer.Inventory.Find(x => x.Name.Contains("fire bomb"));
+         if(temp != null)
        {
         System.Console.WriteLine("You look at the strange round ball in your hands. It has a small pin on it, you decide it would be a good idea to pull the pin(why...they could never find out). You can imagine what happens next. They only thing they found was a small pile of ash.....R.I.P Bob the idiot!");
          Playing = false;
@@ -204,6 +209,20 @@ public void Look(string look)
       System.Console.WriteLine($"You Picked up the {item.Name} ");
       CurrentPlayer.Inventory.Add(item);
       CurrentRoom.Items.Remove(item);
+    }
+    public void WinGame()
+    {
+      System.Console.WriteLine("You decide that it is better for you to blow yourself up and destroy the ship even though you have no idea what it does but it must not be good! You flip the switch and every thing goes white! You have saved the world!");
+        System.Console.WriteLine("YOU WIN kind of! more comming soon!");
+      Playing = false;
+    }
+    public void QuitGame()
+    {
+      Playing = false;
+    }
+    public void Help()
+    {
+      System.Console.WriteLine("You want some help...well ok first off in order to take items just use the word \"take/t \" if you want to use an item use the word \"use/u\". If you want to go somewere use the word \"go/g\"  ");
     }
   }
 }
